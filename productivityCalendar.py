474cfec65c9c25856e220660c8a1ed999e526724
astro-csa <chrisan12999@gmail.com>
2024-12-12 13:55:16 +0100

    Update productivityCalendar.py
    Second commit

--- productivityCalendar.py
+++ productivityCalendar.py
@@ -1,3 +1,6 @@
+import json
+FILE_NAME = "productivityCalendar.json"
+
 class Task:
     def __init__(self,description):
         self.description = description
@@ -26,25 +29,43 @@ class Day:
             for i, task in enumerate(self.tasks,start=1):
                 print(f"{i}) {task}")
     
-    def complete_task(self,task):
-        pass
+    def complete_task(self, task_no):
+        if task_no < 0 | task_no > len(self.tasks):
+            print("There is no task matching the description.")
+        else:
+            self.tasks[task_no - 1].complete()
+        
 
 class Calendar:
     def __init__(self):
-        pass
-
-print("====PRODUCTIVITY CALENDAR====")
-print("Select an option:")
-print("1. Print existing calendars.")
-print("2. Add a calendar.")
-choice = input("Your choice: ")
-
-match choice:
-    case "1":
-        print("You chose option 1.")
-    case "2":
-        print("You chose option 2")
-    case _:
-        print("You are stupid...")
-
-print(f"{choice}")
+        self.days = {}
+    
+    def add_task(self, date, description):
+        if date not in self.days:
+            self.days[date] = Day(date)
+        self.days[date].add_task(description)
+    
+    def complete_task(self, date, task_no):
+        if date not in self.days:
+            print("There is no task for this date.")
+        else:
+            self.days[date].complete_task(task_no)
+            print("Task completed successfully.")
+            
+    def list_all_tasks(self):
+        if not self.days:
+            print("There are not tasks in the calendar.")
+        else:
+            for date, day in self.days.items():
+                print(f"\n{date}")
+                day.list_tasks()
+                
+    def load_calendar(self):
+        try:
+            with open(FILE_NAME, "r") as file:
+                data = json.load(file)
+                print("Calendar sucessfully loaded.")
+                pass
+        except FileNotFoundError:
+            print(f"File {FILE_NAME} not found. A empty calendar will be used.")
+            return {}
